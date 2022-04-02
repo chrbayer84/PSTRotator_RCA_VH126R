19973f4099b1bd68463eb3c78823d0bf6ba66552
Michael Black <mdblack98@yahoo.com>
2022-04-01 23:17:26 -0500

    v1.5 -- cleanup, repeat NEC commands 5 times, timing update seems accurate now Update README

--- sketch_pstrotator/sketch_pstrotator.ino
+++ sketch_pstrotator/sketch_pstrotator.ino
@@ -1,143 +1,171 @@
-#include <IRremote.h>
 #include <Wire.h>
+#include <IRremote.h>
 #include <SerLCD.h>
+#include <EEPROM.h>
 
-// Codes for RCA rotator IR
-/*
-init EF10F906
-LEFT EE11F906
-RIGHT ED12F906
-MEM EC13F906
-A FB04F906
-B fb05F906
-C fb06F906
-D fb07F906
-G fb08F906
-H fb09F906
-I fb0AF906
-J fb0BF906
-K fb0CF906
-L fb0DF906
-*/
-
-#define DISPLAY_ADDRESS1 0x72 //This is the default address of the OpenLCD
-// IR receiver pin
-const int recvPin = 11;
-// IR transmit pin
-const int sendPin = 3;
-IRrecv irrecv(recvPin);
-IRsend irsend(sendPin);
-decode_results results;
-SerLCD lcd;
-
-// Misc items to keep track of
-int azimuth=0;
-int azimuthNew=0;
-char ircode='A';
-int count=0;
-String inputString = "";         // a String to hold incoming data
-int delayTime = 550;  // ms to use for simulating rotation delay
-int debug=0;
+// constants
+// HEADER max 16 characters
+#define HEADER "W9MDB Ham Shack"
+#define HEADER2 "RCA Rotor V1.5"
+#define HEADER3 "W9MDB 2022-04-01"
 // If degreesPin is shorted we'll use 15 degree intervals
 const int degreesPin = 12;
+const int sendPin = 3;
+IRsend irsend(sendPin);
+
+#define PARKADDR 0xff
+
+// variables
 int degreesPer = 30;  // Use 30 for 360 degree and 15 for 180 degree rotation
+int debug = 0;
+int azimuth = 0;
+int azimuthNew = 0;
+int delayTime = 5;
+int delayTimeStartup = 0;
+int startup = 0; // startup time needs to be added for rotor speed sync
+bool moving = 0; // we're moving the antenna
+String inputString;
+String tmpString;
+bool stringComplete;
+char ircode='A';
+SerLCD lcd;
 
-void lcdPrint(String s, int clear)
-{
-  if (clear)
-    Wire.write('-');
-  Wire.write(s.c_str());
-  //Wire.endTransmission();  
-}
+#define PARKED ((unsigned char)0x1f)
+#define NOTPARKED ((unsigned char)0x00)
 
-// the setup function runs once when you press reset or power the board
 void setup() {
   pinMode(degreesPin, INPUT_PULLUP);
-  if (digitalRead(degreesPin) == LOW)
+    if (digitalRead(degreesPin) == LOW)
   {
     degreesPer = 15;  
   }
-
+  // LCD stuff
+  Serial.begin(4800);
+  inputString.reserve(8);
   Wire.begin();
   lcd.begin(Wire);
-  //Wire.beginTransmission(DISPLAY_ADDRESS1);
-  //Wire.write('|');
-  //lcdClear();
-  lcd.write("RCA controller v1.0");
+  delay(500);
+  lcd.clear();
+  lcd.print(HEADER2);
   lcd.setCursor(0,1);
-  lcd.write("By W9MDB 2021-09-07");
-  lcd.setCursor(0,2);
-  lcd.write("EZRotor compatible");
-  delay(2000);
-  Serial.begin(4800);
-  lcd.setCursor(0,2);
-  lcd.write("                  ");
-  if (debug)
-  {
-    Serial.println("EZRotor to RCA controller v1.0 by W9MDB");
-  }
-
-  inputString.reserve(8);
-    
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  irrecv.begin(recvPin, ENABLE_LED_FEEDBACK, USE_DEFAULT_FEEDBACK_LED_PIN);
+  lcd.print(HEADER3);
   IrSender.begin(sendPin, ENABLE_LED_FEEDBACK); 
-  delay(1000);
-  park();
+  delay(3000);
+
+  parked();
+  if (!parked())
+  {
+    park();
+  }
+  else
+  {
+    lcd.clear();
+    lcd.write(HEADER);
+    lcd.setCursor(0,1);
+    lcd.write("Parked");
+  }
+  EEPROM_writePark(PARKED);
 }
 
+bool parked()
+{
+ unsigned char value;
+ EEPROM.get(PARKADDR, value);
+ lcd.print("\nParked="+(String)value);
+ return value == PARKED;
+}
+
+void park()
+{
+    lcd.clear();
+    lcd.write("Park");
+    if (debug) Serial.println("INIT Antenna to azimuth=0\r\n");
+    sendNEC(0x6, 0x10, 0x05);
+    azimuth = 340;
+    azimuthNew = 0;
+}
+
+void EEPROM_writePark(unsigned char value)
+{
+ EEPROM.put(PARKADDR, value);
+}
+
+void sendNEC(unsigned char c1, unsigned char c2, unsigned char c3)
+{
+  int i;
+  noInterrupts();
+  delay(100);
+  for(i=0;i<5;++i)
+  {
+    IrSender.sendNEC(c1,c2,c3);
+    delay(50);
+  }
+  interrupts();
+}
+
+
 void serialEvent() {
-  int icmd;
-  
   while (Serial.available()) {
     char inChar = (char)Serial.read();
-    inputString += inChar;
-    if (debug)
-    {
-      Serial.write(inChar);
-      Serial.flush();
+    if (inChar == 0x0a || inChar == 0x0d) continue;
+    tmpString += inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == ';') {
+      if (inputString == "")
+      {
+        setString(tmpString);
+        tmpString = "";
+      }
+      else
+      {
+        if (debug) Serial.println("Dropping %s" + tmpString);
+        tmpString = ""; // drop it if we didn't process last one
+      }
     }
-    if (inputString.length() > 8) inputString = ""; // something is wrong so drop it.
+  }
+}
 
-    if (inChar == ';' || inChar == 0x0d)
+void setString(String s)
+{
+  inputString = s;
+  stringComplete = true;
+}
+
+void ezrotor(String cmd)
+{
+  int icmd;
+  if (debug) Serial.println("ezrotor " + cmd);
+  if (sscanf(cmd.c_str(),"AP1%d",&azimuthNew) == 1)
+  {
+    // bidirectional loop only has to move 0-180
+    if (degreesPer == 15) azimuthNew = azimuthNew % 180;
+    move(azimuthNew);
+    // simulate rotation speed
+    ircode = az2char(azimuthNew);
+    azimuthNew = (ircode-'A')*degreesPer;
+    if (azimuthNew > azimuth) azimuth += 10;
+    else azimuth -= 10;
+    if (azimuth < 0) azimuth = 0;
+    if (abs(azimuth - azimuthNew) < 15) azimuth = azimuthNew;
+  }
+  else if (sscanf(cmd.c_str(),"AI%d",&icmd)==1)
+  {
+    char cmd[8];
+    switch(icmd)
     {
-      if ((inputString.length() == 7) && (sscanf(inputString.c_str(),"AP1%d",&azimuthNew) == 1))
-      {
-        // bidirectional loop only has to move 0-180
-        if (degreesPer == 15) azimuthNew = azimuthNew % 180;
-        inputString = "";
-        move(azimuthNew);
-        // simulate rotation speed
-        ircode = az2char(azimuthNew);
-        azimuthNew = (ircode-'A')*degreesPer;
-        if (azimuthNew > azimuth) azimuth += 10;
-        else azimuth -= 10;
-        if (azimuth < 0) azimuth = 0;
-        if (abs(azimuth - azimuthNew) < 15) azimuth = azimuthNew;
-      }
-      else if (sscanf(inputString.c_str(),"AI%d;",&icmd)==1)
-      {
-        inputString = "";
-        char cmd[8];
-  
-        switch(icmd)
-        {
-        case 1:
-        sprintf(cmd,";%03d",azimuth);
-        Serial.print(cmd);
-        break;
-        default:
-        break;
-        // print error to display
-        }
-      }
-      else if (inputString.indexOf(';') > 0)
-      {
-        inputString = "";
-      }
+    case 1:
+      sprintf(cmd,";%03d",azimuth);
+      Serial.print(cmd);
+      break;
+    default:
+      break;
+      // print error to display
     }
+  }
+  else if (inputString.indexOf(';') > 0)
+  {
+    inputString = "";
   }
 }
 
@@ -152,63 +180,72 @@ char az2char(int azimuth)
   return letter;
 }
 
-void park()
+void move(int azimuth)
 {
-    lcd.setCursor(0,2);
-    lcd.write("Cmd: INIT");
-    lcd.setCursor(0,3);
-    lcd.write("Position: 0      ");
-    if (debug) Serial.println("INIT Antenna to azimuth=0\r\n");
-    IrSender.sendNEC(0x6, 0x10, 0x05);
-    azimuth = 360;
-    azimuthNew = 0;
+  startup = 1;
+  char letter = az2char(azimuth);
+  int cmd = 0x04 + (letter - 'A');
+  if (debug) {Serial.print("Move to ");Serial.print(azimuth);Serial.print("=");Serial.println(letter);}
+  sendNEC(0x6, cmd, 5);
+  //lcd.setCursor(0,0);
+  //lcd.write("                ");
+  //lcd.setCursor(0,0);
+  lcd.clear();
+  lcd.write("Move to ");lcd.print(azimuth);lcd.print("  ");
+  position();
+  EEPROM_writePark(NOTPARKED);
 }
 
 void position()
 {
-  lcd.setCursor(0,3);
-  lcd.write("Position: ");
-  lcd.write(ircode);
-  lcd.write("=");
+  lcd.setCursor(0,1);
+  lcd.write("Az ");
   lcd.write(String(azimuth).c_str());
-  lcd.write("   ");
-}
-void move(int azimuth)
-{
-  char letter = az2char(azimuth);
-  int cmd = 0x04 + (letter - 'A');
-  if (debug) {Serial.print("Move to ");Serial.print(azimuth);Serial.print("=");Serial.println(letter);}
-  IrSender.sendNEC(0x6, cmd, 5);
-  lcd.setCursor(0,2);
-  lcd.write("Move to ");lcd.print(azimuth);lcd.write("   ");
-  position();
+  lcd.write("  ");
+  lcd.setCursor(10,1);
+  lcd.write("Code=");
+  lcd.write(ircode);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  
-#if 0 // only if we want to see the codes being transmitted
-  if (irrecv.decode())
+  if (stringComplete)
   {
-    //Serial.print("IR Recv: ");
-    //irrecv.printIRResultShort(&Serial);
-    //Serial.println(results.value, HEX);
-    //Serial.flush();
-    irrecv.resume();
+    String myString = inputString;
+    stringComplete = false;
+    inputString = "";
+    if (debug) Serial.println(myString);
+    ezrotor(myString);
   }
-#endif
   if (azimuth < azimuthNew)
   {
-    azimuth += 10;
-    if (azimuth > azimuthNew) azimuth= azimuthNew;
+    moving = true;
+    if (azimuth > azimuthNew) azimuth = azimuthNew;
+    azimuth += 1;
     position();
-    delay(delayTime);  // timing approximates tuner rotation time
+    delay(delayTime+startup*delayTimeStartup);  // timing approximates tuner rotation time
+    startup = 0;
   }
   else if (azimuth > azimuthNew)
   {
-    azimuth -= 10;
-    if (azimuth < azimuthNew) azimuth= azimuthNew;
+    moving = true;
+    if (azimuth < azimuthNew) azimuth = azimuthNew;
+    azimuth -= 1;
     position();
-    delay(delayTime);
+    delay(delayTime+startup*delayTimeStartup);  // timing approximates tuner rotation time
+    startup = 0;
+  }
+  else if (moving)
+  {
+    moving = false;
+    lcd.setCursor(0,0);
+    lcd.write(HEADER);
+  }
+  if (azimuth == 0) 
+  {
+    EEPROM_writePark(PARKED);
+  }
+  else
+  {
+    EEPROM_writePark(NOTPARKED);
   }
 }
